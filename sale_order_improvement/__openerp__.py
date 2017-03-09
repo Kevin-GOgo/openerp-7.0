f87d0d3c5b2cec2678e15879ec5d95d3f4d53ec8
kevin <kevin.huang@elico-corp.com>
2017-03-08 19:53:46 -0800

    modify

--- sale_order_improvement/__openerp__.py
+++ sale_order_improvement/__openerp__.py
@@ -1,29 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2011 Elico Corp. All Rights Reserved.
-#    Author:            Eric CAUDAL <contact@elico-corp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014 Elico corp(www.elico-corp.com)
+# Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
 
 
 {
-    'name': 'Sales Order form improvement',
-    'version': '1.0',
+    'name': 'Sales Order improvement',
+    'version': '7.0.1.0.0',
     'category': 'Generic Modules/Base',
     'description': """
 Extension to module sale to improve the order of the UoM in the sales
@@ -31,11 +13,7 @@ order lines form.  """,
     'author': 'Elico Corp',
     'website': 'http://www.openerp.net.cn',
     'depends': ['sale'],
-    'init_xml': [],
     'update_xml': ['sale_order_improvement_view.xml'],
-    'demo_xml': [],
-    'test': [],
     'installable': True,
     'active': False,
-    'certificate': '',
 }
